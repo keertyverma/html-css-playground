be16fbfc3fc457ea8c08eeb35a19b3bc03fb9ff8
Keerty Verma <keertyverma1303@gmail.com>
2023-03-22 18:15:56 +0530

    add dice game

---
+++ dice_game/README.md
@@ -0,0 +1,5 @@
+# Dice Game
+
+![player-1-wins](images/player-1-wins.png)
+![player-2-wins](images/player-2-wins.png)
+![draw](images/draw.png)
---
+++ dice_game/index.js
@@ -0,0 +1,19 @@
+// Generate random number for both dice between 1 to 6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+// Change dice image according to the random number
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img1").setAttribute("src", randomDiceImage1);
+document.querySelector(".img2").setAttribute("src", randomDiceImage2);
+
+// Decide who wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
